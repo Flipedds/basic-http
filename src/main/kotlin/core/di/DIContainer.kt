04b63257e4be69fee42220ffd2e48a68bd1202e1
Filipe André <flipeandre12@gmail.com>
2025-12-13 06:34:01 -0300

    :sparkles: [improvement]: improve di container

--- src/main/kotlin/core/di/DIContainer.kt
+++ src/main/kotlin/core/di/DIContainer.kt
@@ -1,7 +1,7 @@
 package core.di
 
-import common.LogColors
 import common.BasicLog
+import common.LogColors
 import core.domain.di.Injectable
 import io.github.classgraph.ClassGraph
 import io.github.classgraph.ClassInfo
@@ -10,42 +10,40 @@ import java.lang.reflect.Constructor
 import kotlin.jvm.Throws
 
 /**
- * This class is responsible for creating and managing dependency injection.
- * It uses reflection to create beans and inject them into the classes that need them.
- * It scans the project for classes annotated with @Injectable and creates beans for them.
- * It also scans for classes that implement interfaces and creates beans for them.
- * It stores the beans in a map and provides them when needed.
- * It also logs the beans created.
+ * This class is responsible for creating and managing dependency injection. It uses reflection to
+ * create beans and inject them into the classes that need them. It scans the project for classes
+ * annotated with @Injectable and creates beans for them. It also scans for classes that implement
+ * interfaces and creates beans for them. It stores the beans in a map and provides them when
+ * needed. It also logs the beans created.
  */
 @Suppress("UNCHECKED_CAST")
 object DIContainer {
     private val beanMap: MutableMap<Class<*>, Any> = HashMap()
+    private val interfacesMap: MutableMap<String, String> = HashMap()
 
     fun initializeDI() {
         beanMap.clear()
-        ClassGraph()
-            .enableAllInfo()
-            .acceptPackages()
-            .scan()
-            .use { scanResult: ScanResult ->
-                scanResult
-                    .getClassesWithAnyAnnotation(Injectable::class.java.name)
-                    .forEach { classWithInjectable: ClassInfo ->
-                        getBean(
-                            classWithInjectable.loadClass()
-                        )
-                    }
+        interfacesMap.clear()
+        ClassGraph().enableAllInfo().acceptPackages().scan().use { scanResult: ScanResult ->
+            scanResult.allInterfaces.forEach { interfaceInfo ->
+                scanResult.getClassesImplementing(interfaceInfo.name).firstOrNull()?.let { impl ->
+                    interfacesMap[interfaceInfo.name] = impl.name
+                }
             }
 
-        BasicLog.getLogWithColorFor<DIContainer>(
-            LogColors.BLUE, " <<<< ------ BEANS ---- >>>> ")
+            scanResult.getClassesWithAnyAnnotation(Injectable::class.java.name).forEach {
+                    classWithInjectable: ClassInfo ->
+                getBean(classWithInjectable.loadClass())
+            }
+        }
+
+        BasicLog.getLogWithColorFor<DIContainer>(LogColors.BLUE, " <<<< ------ BEANS ---- >>>> ")
 
         beanMap.forEach { (clazz, any) ->
-            BasicLog.getLogWithColorFor<DIContainer>(
-            LogColors.BLUE, "${clazz.simpleName} -> $any") }
+            BasicLog.getLogWithColorFor<DIContainer>(LogColors.BLUE, "${clazz.simpleName} -> $any")
+        }
 
-        BasicLog.getLogWithColorFor<DIContainer>(
-            LogColors.BLUE," <<<< ----------------- >>>> ")
+        BasicLog.getLogWithColorFor<DIContainer>(LogColors.BLUE, " <<<< ----------------- >>>> ")
     }
 
     @Throws(NotImplementedError::class)
@@ -54,17 +52,12 @@ object DIContainer {
         val keyForBeanClass = toBeInjectedClass
 
         if (toBeInjectedClass.isInterface) {
-            toBeInjectedClass = Class.forName(
-                ClassGraph()
-                    .enableAllInfo()
-                    .acceptPackages()
-                    .scan()
-                    .let { scanResult: ScanResult ->
-                        scanResult
-                            .getClassesImplementing(keyForBeanClass.name)
-                            .firstOrNull()?.name
-                            ?: throw NotImplementedError("Interface: ${keyForBeanClass.simpleName} not implemented")
-                    })
+            val implementationClassName =
+                    interfacesMap[keyForBeanClass.name]
+                            ?: throw NotImplementedError(
+                                    "Interface: ${keyForBeanClass.simpleName} not implemented"
+                            )
+            toBeInjectedClass = Class.forName(implementationClassName)
         }
 
         val constructor: Constructor<*> = toBeInjectedClass.constructors.first()
